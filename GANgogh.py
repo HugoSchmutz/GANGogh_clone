a5bb9ef00eb8ec66dfb48d7a3aba7985e271731e
HugoSchmutz <33659418+HugoSchmutz@users.noreply.github.com>
2020-01-12 14:52:21 +0100

    Update GANgogh.py

--- GANgogh.py
+++ GANgogh.py
@@ -29,7 +29,7 @@ BATCH_SIZE = 84 # Batch size. Must be a multiple of CLASSES and N_GPUS
 ITERS = 200000 # How many iterations to train for
 LAMBDA = 10 # Gradient penalty lambda hyperparameter
 OUTPUT_DIM = 64*64*3 # Number of pixels in each iamge
-CLASSES = 14 #Number of classes, for genres probably 14
+CLASSES = 1 #Number of classes, for genres probably 1
 PREITERATIONS = 2000 #Number of preiteration training cycles to run
 lib.print_model_settings(locals().copy())
 
