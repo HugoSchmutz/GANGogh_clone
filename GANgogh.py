88e964ae85ebbe33b224ee1fd0623db479299231
HugoSchmutz <33659418+HugoSchmutz@users.noreply.github.com>
2020-01-14 17:35:09 +0100

    Update GANgogh.py

--- GANgogh.py
+++ GANgogh.py
@@ -168,11 +168,11 @@ def kACGANGenerator(n_samples, numClasses, labels, noise=None, dim=DIM, bn=True,
     print("")
     output = pixcnn_gated_nonlinearity('Generator.nl4', 2*dim, output[:,::2], output[:,1::2], condition[:,::2], condition[:,1::2])
     
-    output = lib.ops.deconv2d.Deconv2D('Generator.5', 2*dim, dim*2, 5, output)
+    output = lib.ops.deconv2d.Deconv2D('Generator.5', dim, dim*2, 5, output)
     if bn:
         output = Batchnorm('Generator.BN5', [0,2,3], output)
     condition = lib.ops.linear.Linear('Generator.cond4', numClasses, 2*64*64*dim*2, labels)
-    condition = tf.reshape(condition, [-1, 1*dim*2, 64, 64])
+    condition = tf.reshape(condition, [-1, 1*dim, 64, 64])
     print(output.shape)
     print(condition.shape)
     print("")
